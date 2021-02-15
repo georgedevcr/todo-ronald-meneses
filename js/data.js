4dc365cedeab79246a9ea1edc217371aef914fe0
remscr <ronald-e-@hotmail.com>
2021-02-14 22:13:37 -0600

    entregable 3: agregar nuevo todo, mensaje de acción

--- js/data.js
+++ js/data.js
@@ -3,33 +3,49 @@ let todoList = [
 	{
 		idTodo: 1,
 		description: "Crear html para mostrar el listado de los todo",
-		idState: 1,
-		state: "Finalizado",
-		idUser: 1,
-		username: "remscr"
+		state: {
+			id: 2,
+			description: "Finalizado",
+		},
+		user: {
+			id: 1,
+			username: "remscr"
+		}
 	},
 	{
 		idTodo: 2,
 		description: "Generar estructura de objeto todo",
-		idState: 1,
-		state: "Finalizado",
-		idUser: 1,
-		username: "remscr"
+		state: {
+			id: 2,
+			description: "Finalizado",
+		},
+		user: {
+			id: 1,
+			username: "remscr"
+		}
 	},
 	{
 		idTodo: 3,
 		description: "Crear método para agregar a la tabla del listado que se muestra al usuario",
-		idState: 1,
-		state: "Finalizado",
-		idUser: 1,
-		username: "remscr"
+		state: {
+			id: 2,
+			description: "Finalizado",
+		},
+		user: {
+			id: 1,
+			username: "remscr"
+		}
 	},
 	{
 		idTodo: 4,
 		description: "Establecer color para los botones según el estado",
-		idState: 2,
-		state: "Pendiente",
-		idUser: 0,
-		username: "Invitado"
+		state: {
+			id: 1,
+			description: "Pendiente",
+		},
+		user: {
+			id: 0,
+			username: "Invitado"
+		}
 	}
 ]
--- js/todo.js
+++ js/todo.js
@@ -2,38 +2,103 @@
 
 const idTblTodo = 'tbl-todo';
 
+const classesAlert = {
+	danger: "alert-danger",
+	succes: "alert-success"
+}
+
 const states = {
-	done: 1,
-	canceled: 2,
-	suspended: 3
+	pending: {
+		id: 1,
+		description: "Pendiente"
+	},
+	done: {
+		id: 2,
+		description: "Finalizado"
+	},
+	canceled: {
+		id: 3,
+		description: "Cancelado"
+	},
+	suspended: {
+		id: 4,
+		description: "Suspendido"
+	}
 }
 
 const todoTemplate = {
-	geTemplate: function (todo) {
-		console.log(todo);
-		
+	GetTemplate: function (todo) {
 		return `<tr>
 					<td>${todo.description}</td>
-					<td>${todo.state}</td>
-					<td>${todo.username}</td>
+					<td>${todo.state.description}</td>
+					<td>${todo.user.username}</td>
 					<td>
 						<div class="justify-icons icons-actions">
-							<a onclick="CheckToDo(${todo.idTodo})" href="#" class="${todo.idState == states.done ? 'teal-text darken-4' : 'black-text'}"><i class="material-icons">check</i></a>
-							<a onclick="CancelToDo(${todo.idTodo})" href="#" class="${todo.idState == states.canceled ? 'teal-text darken-4' : 'black-text'}"><i class="material-icons">clear</i></a>
-							<a onclick="SuspendToDo(${todo.idTodo})" href="#" class="${todo.idState == states.suspended ? 'teal-text darken-4' : 'black-text'}"><i class="material-icons">timer_off</i></a>
+							<a onclick="CheckToDo(${todo.idTodo})" href="#" class="${todo.state.id == states.done.id ? 'teal-text darken-4' : 'black-text'}"><i class="material-icons">check</i></a>
+							<a onclick="CancelToDo(${todo.idTodo})" href="#" class="${todo.state.id == states.canceled.id ? 'teal-text darken-4' : 'black-text'}"><i class="material-icons">clear</i></a>
+							<a onclick="SuspendToDo(${todo.idTodo})" href="#" class="${todo.state.id == states.suspended.id ? 'teal-text darken-4' : 'black-text'}"><i class="material-icons">timer_off</i></a>
 						</div>
 					</td>
 					<td>${todo.idUser}</td>
 				</tr>`;
+	},
+	GetNewTodo: function(description)
+	{
+		let newTodo = {
+			idTodo: (todoList.length + 1), // cambiar y buscar el mayor dentro del listado
+			description: description,
+			state: states.pending,
+			user: {
+				idUser: 0,
+				username: "Invitado"
+			}
+		};
+		return newTodo;
 	}
 }
 
 function LoadTableTodo(lstTodo){
 	const tbody = document.getElementById(idTblTodo).querySelector("tbody");
-	
+	tbody.innerHTML = "";
 	for (let i = 0; i < lstTodo.length; i++) {
 		const element = lstTodo[i];
-		tbody.insertAdjacentHTML('beforeend', todoTemplate.geTemplate(element));
+		tbody.insertAdjacentHTML('beforeend', todoTemplate.GetTemplate(element));
+	}
+}
+
+let FrmTodo = {
+	idFrm: 'frm-todo',
+	idTxtTodo: 'new-todo',
+	get txtTodo() {
+		return document.getElementById(this.idTxtTodo).value;
+	},
+	set txtTodo(value) {
+		document.getElementById(this.idTxtTodo).value = value;
+	},
+	Open : function(){
+		//Mostrar el formulario para crear el todo y limpiar el formulario
+		this.Clean();
+		document.getElementById(this.idFrm).hidden = false;
+	},
+	Close : function(){
+		this.Clean();
+		document.getElementById("alert-action").hidden = true;
+		document.getElementById(this.idFrm).hidden = true;
+	},
+	Clean: function()
+	{
+		this.txtTodo = '';
+	},
+	Save: function()
+	{
+		if(this.txtTodo != ""){
+			todoList.push(todoTemplate.GetNewTodo(this.txtTodo));
+			LoadTableTodo(todoList);
+			this.Close();
+			ShowActionAlert("ToDo registrado correctamente.", classesAlert.succes);
+		}
+		else
+			ShowActionAlert("Ingrese la descripción del ToDo.", classesAlert.danger);
 	}
 }
 
@@ -47,6 +112,26 @@ function CancelToDo(idTodo){
 
 function SuspendToDo(idTodo){
 	console.log(idTodo);
+}
+
+function CloseAlert(btn)
+{
+	let divAlert = btn.closest("div.alert");
+	divAlert.hidden = true;
+}
+
+function ShowActionAlert(message, type)
+{
+	let alert = document.getElementById("alert-action");
+	alert.removeAttribute("class");
+	alert.classList.add("alert");
+	alert.classList.add("alert-dismissible");
+	alert.classList.add(type);
+
+	let divMessageAlert = alert.querySelector("span");
+	divMessageAlert.textContent = message;
+
+	alert.hidden = false;
 }
 
 document.body.onload = function()
