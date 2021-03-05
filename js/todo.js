1d5cd8fe5d46991524c36bbfcefdc37fa92e3f5e
remscr-uc <ronald.meneses.sanchez@ucreativa.com>
2021-03-04 20:58:36 -0600

    entregable final

--- js/todo.js
+++ js/todo.js
@@ -1,22 +1,46 @@
 let db = firebase.firestore();
 
-const idTblTodo = 'tbl-todo';
-
-const classesAlert = {
-	danger: "alert-danger",
-	succes: "alert-success"
+const Alert = {
+	el: 'alert-action',
+	timeout: null,
+	types: {
+		danger: "alert-danger",
+		success: "alert-success"
+	},
+	Show(config){
+		let alert = document.getElementById(this.el);
+		alert.removeAttribute("class");
+		alert.classList.add("alert");
+		alert.classList.add("alert-dismissible");
+		alert.classList.add(config.type);
+	
+		let divMessageAlert = alert.querySelector("span");
+		divMessageAlert.textContent = config.message;
+		alert.hidden = false;
+		clearTimeout(this.timeout);
+		this.timeout = setTimeout(() => {
+			alert.hidden = true;
+		}, 5000);
+	},
+	Close(btn)
+	{
+		let divAlert = btn.closest("div.alert");
+		divAlert.hidden = true;
+	}
 }
 
-
 let states = {};
-
 const statesCollection = "states_mod";
-const onGetStatesFS = (success, error) => db.collection(statesCollection).get().then(success);
+const onGetStatesFS = (success) => db.collection(statesCollection).get().then(success);
 
 const TodoTemplate = {
 	collection: "todos",
 	idtxtSearch: 'sch-txt-todo',
 	idCbSearch: 'sch-cb-state',
+	idTblTodo: 'tbl-todo',
+	get txtSearch() {
+		return document.getElementById(this.idtxtSearch).value;
+	},
 	GetTemplate: function (idTodo, todo) {
 		return `<tr>
 					<td>${todo.description}</td>
@@ -25,10 +49,10 @@ const TodoTemplate = {
 					<td>
 						<div class="icons-actions">
 							<a onclick="TodoTemplate.UpdateState('${idTodo}', ${states.done.id})" href="#" class="${todo.state.id == states.done.id ? 'teal-text darken-4' : 'black-text'}"><i class="material-icons">check</i></a>
-							<a onclick="TodoTemplate.UpdateState('${idTodo}', ${states.canceled.id})" href="#" class="${todo.state.id == states.canceled.id ? 'teal-text darken-4' : 'black-text'}"><i class="material-icons">clear</i></a>
+							<a onclick="TodoTemplate.UpdateState('${idTodo}', ${states.canceled.id})" href="#" class="${todo.state.id == states.canceled.id ? 'teal-text darken-4' : 'black-text'}"><i class="material-icons">do_not_disturb_alt</i></a>
 							<a onclick="TodoTemplate.UpdateState('${idTodo}', ${states.suspended.id})" href="#" class="${todo.state.id == states.suspended.id ? 'teal-text darken-4' : 'black-text'}"><i class="material-icons">timer_off</i></a>
-							<!-- <a onclick="FrmTodo.EditTodo('${idTodo}')" href="#" class="black-text"><i class="material-icons">edit</i></a>-->
-							<!-- <a onclick="TodoTemplate.RemoveTodo('${idTodo}')" href="#" class="black-text"><i class="material-icons">remove</i></a>-->
+							<a onclick="FrmTodo.EditTodo('${idTodo}')" href="#" class="black-text"><i class="material-icons">edit</i></a>
+							<a onclick="TodoTemplate.RemoveTodo('${idTodo}')" href="#" class="black-text"><i class="material-icons">delete_forever</i></a>
 						</div>
 					</td>
 					<td>${todo.idUser}</td>
@@ -37,71 +61,70 @@ const TodoTemplate = {
 	GetNewTodo: function(description)
 	{
 		let newTodo = {
-			//idTodo: this.FindMaxId(todoList), // (todoList.length + 1), // cambiar y buscar el mayor dentro del listado
 			description: description,
 			state: states.pending,
 			user: {
 				idUser: 0,
 				username: "Invitado"
-			}
+			},
+			createdAt : firebase.firestore.FieldValue.serverTimestamp(),
+			updatedAt : firebase.firestore.FieldValue.serverTimestamp()
 		};
 		return newTodo;
 	},
 	LoadTableTodo: function(){
-		db.collection(this.collection).onSnapshot((query) =>{
-			const tbody = document.getElementById(idTblTodo).querySelector("tbody");
+		let self = this;
+		db.collection(this.collection).orderBy("updatedAt", "desc").onSnapshot((query) =>{
+			const tbody = document.getElementById(self.idTblTodo).querySelector("tbody");
 			tbody.innerHTML = "";
 			query.forEach(element => {
 				tbody.insertAdjacentHTML('beforeend', TodoTemplate.GetTemplate(element.id, element.data()));
 			});
 		});
 	},
-	FindMaxId: function(lstTodo)
-	{
-		let idMax = 0;
-		if(lstTodo.length > 0)
-		{
-			const todoMax = lstTodo.reduce(function(prev, current) {
-				return (prev.idTodo > current.idTodo) ? prev : current
+	UpdateTodo: function(idTodo, todo){
+		todo.state = states.pending;
+		todo.updatedAt = firebase.firestore.FieldValue.serverTimestamp();
+		db.collection(this.collection).doc(idTodo).update(
+			todo
+		).then(() => {
+			Alert.Show({ 
+				message: "ToDo actualizado correctamente.", 
+				type: Alert.types.success
 			});
-			idMax = todoMax.idTodo;
-		}	
-		return idMax + 1;
-	},
-	FindTodo: function(idTodo){
-		return todoList.find(t => t.idTodo == idTodo);
-	},
-	UpdateTodo: function(todo){
-		let todoToUpdate = todoList.find(t => t.idTodo == todo.idTodo);
-		todoToUpdate = todo;
-		todoToUpdate.state = states.pending;
+		}).catch((error) => {
+			Alert.Show({
+				message: "No se pudo actualizar el ToDo.", 
+				type: Alert.types.danger
+			});
+		});
 	},
 	RemoveTodo: function(idTodo)
 	{
-		for (let index = 0; index < todoList.length; index++) {
-			const element = todoList[index];
-			if(element.idTodo == idTodo){
-				todoList.splice(index, 1);
-				break;
-			}
-		}
-		//this.LoadTableTodo(todoList);
+		db.collection(this.collection).doc(idTodo).delete().then(() => {
+			Alert.Show({
+				message: "ToDo eliminado correctamente.", 
+				type: Alert.types.success
+			});
+		}).catch((error) => {
+			Alert.Show({
+				message: "No se pudo eliminar el ToDo.", 
+				type: Alert.types.danger
+			});
+		});
 	},
 	UpdateState: function(idTodo, idState)
 	{
 		let newState = Object.values(states).find(t => t.id == idState);
-		console.log(idTodo);
-		
-		db.collection(this.collection).doc(idTodo).update({ "state" : newState });
-		// let todoToUpdate = todoList.find(t => t.idTodo == idTodo);
-		
-		// todoToUpdate.state = newState;
-		// //this.LoadTableTodo(todoList);
+		let todo = {};
+		todo.updatedAt = firebase.firestore.FieldValue.serverTimestamp();
+		todo.state = newState;
+		db.collection(this.collection).doc(idTodo).update(todo);
 	},
 	InitFilters: function()
 	{
 		//Filtro de estados
-		var cbState = document.getElementById('sch-cb-state');
+		let cbState = document.getElementById('sch-cb-state');
 		for (let index = 0; index < Object.values(states).length; index++) {
 			const element = Object.values(states)[index];
 			const option = document.createElement("option");
@@ -109,17 +132,34 @@ const TodoTemplate = {
 			option.text = element.description;
 			cbState.add(option);
 		}
-		var instances = M.FormSelect.init(cbState);
+		M.FormSelect.init(cbState);
 	},
 	SearchTodo: function()
 	{
-		let txtTodoSearch = document.getElementById(this.idtxtSearch).value;
-		let cbSearch = document.getElementById(this.idCbSearch).value;
-		let todoBusqueda = todoList.filter(t => t.description.includes(txtTodoSearch));
-		//this.LoadTableTodo(todoBusqueda);
+		let self = this;
+		let statesSch = M.FormSelect.getInstance(document.getElementById('sch-cb-state')).getSelectedValues();
+		let statesObjSch = [];
+		for (let index = 0; index < statesSch.length; index++) {
+			const element = statesSch[index];
+			if(element == "")
+				continue;
+			let state = Object.values(states).find(t => t.id == element);
+			statesObjSch.push(state);
+		}
+		statesObjSch = statesObjSch.length > 0 ? statesObjSch : Object.values(states);
+		//let txtTodoSearch = document.getElementById(this.idtxtSearch).value;
+		let todosRef = db.collection(this.collection).orderBy("updatedAt", "desc").where("state", "in", statesObjSch);
+		todosRef.onSnapshot((query) =>{
+			const tbody = document.getElementById(self.idTblTodo).querySelector("tbody");
+			tbody.innerHTML = "";
+			query.forEach(element => {
+				//Aplicar filtro de description
+				if(self.txtSearch == "" || element.data().description.includes(self.txtSearch))
+					tbody.insertAdjacentHTML('beforeend', self.GetTemplate(element.id, element.data()));
+			});
+		});
 	}
 }
-
 
 let FrmTodo = {
 	idFrm: 'frm-todo',
@@ -135,18 +175,23 @@ let FrmTodo = {
 	},
 	NewTodo: function()
 	{
-		//Limpiar el formulario
 		document.getElementById(this.idLabel).textContent = "Nuevo ToDo";
 		this.Clean();
 		this.Open();
 	},
 	EditTodo: function(idTodo)
 	{
-		let todo = TodoTemplate.FindTodo(idTodo);
-		this.idTodo = todo.idTodo;
-		this.txtTodo = todo.description;
-		document.getElementById(this.idLabel).textContent = "Editar ToDo";
-		this.Open();
+		let self = this;
+		db.collection(TodoTemplate.collection)
+		.doc(idTodo)
+		.get()
+		.then((docRef) => { 
+			let data = docRef.data();
+			self.idTodo = idTodo;
+			self.txtTodo = data.description;
+			document.getElementById(this.idLabel).textContent = "Editar ToDo";
+			self.Open();
+		});
 	},
 	Open : function(){
 		//Mostrar el formulario para crear el todo 
@@ -154,7 +199,6 @@ let FrmTodo = {
 	},
 	Close : function(){
 		this.Clean();
-		document.getElementById("alert-action").hidden = true;
 		document.getElementById(this.idFrm).hidden = true;
 	},
 	Clean: function()
@@ -168,58 +212,27 @@ let FrmTodo = {
 		if(self.txtTodo != ""){
 			if(self.idTodo == null)
 			{
-				console.log(self.txtTodo);
 				let todo = TodoTemplate.GetNewTodo(self.txtTodo);
 				db.collection(TodoTemplate.collection).doc().set(todo);
-				ShowActionAlert("ToDo registrado correctamente.", classesAlert.succes);
+				Alert.Show({
+					message: "ToDo registrado correctamente.", 
+					type: Alert.types.success
+				});
 			}
 			else{
-				let todo = TodoTemplate.FindTodo(self.idTodo);
+				let todo = {};
 				todo.description = self.txtTodo;
-				TodoTemplate.UpdateTodo(todo);
-				ShowActionAlert("ToDo actualizado correctamente.", classesAlert.succes);
+				TodoTemplate.UpdateTodo(self.idTodo, todo);
 			}
-			//TodoTemplate.LoadTableTodo(todoList);
 			self.Close();
 		}
 		else
-			ShowActionAlert("Ingrese la descripción del ToDo.", classesAlert.danger);
+			Alert.Show({ 
+				message: "Ingrese la descripción del ToDo.", 
+				type: Alert.types.danger
+			});
 	}
 }
-
-function CheckToDo(idTodo){
-	console.log(idTodo);
-}
-
-function CancelToDo(idTodo){
-	console.log(idTodo);
-}
-
-function SuspendToDo(idTodo){
-	console.log(idTodo);
-}
-
-function CloseAlert(btn)
-{
-	let divAlert = btn.closest("div.alert");
-	divAlert.hidden = true;
-}
-
-function ShowActionAlert(message, type)
-{
-	let alert = document.getElementById("alert-action");
-	alert.removeAttribute("class");
-	alert.classList.add("alert");
-	alert.classList.add("alert-dismissible");
-	alert.classList.add(type);
-
-	let divMessageAlert = alert.querySelector("span");
-	divMessageAlert.textContent = message;
-
-	alert.hidden = false;
-}
-
-
 
 /**
  * Al iniciar el documento, ejecutar
@@ -227,7 +240,7 @@ function ShowActionAlert(message, type)
  */
 document.body.onload = async function()
 {
-	//Obtener los estados para validar contra la información que tiene la propiedad del todo
+	//Obtener los estados de la base para validar contra la información que tiene la propiedad del todo
 	await onGetStatesFS((q) =>
 	{
 		q.forEach((s) =>
